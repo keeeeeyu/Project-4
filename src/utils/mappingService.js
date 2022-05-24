e8c7beac886d8e8462e738f5395071c36c6e6f0a
Kee Yu <keeeeeyu@gmail.com>
2022-05-24 10:22:00 -0700

    na filter added

--- src/utils/mappingService.js
+++ src/utils/mappingService.js
@@ -11,9 +11,15 @@ function filterAfrica(continent) {
 function filterAsia(continent) {
     const asData = continent.filter(countries => countries.continent === 'Asia')
 }
+
+function filterNorthAmerica(continent) {
+    const naData = continent.filter(countries => countries.continent === 'North America')
+}
 const mappingService = {
   filterEurope,
   filterAfrica,
+  filterAsia,
+  filterNorthAmerica,
 };
 
 export default mappingService;
