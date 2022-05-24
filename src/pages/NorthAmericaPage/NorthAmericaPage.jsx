58fff85f9060eeb027240a450921499b0eef05f2
Kee Yu <keeeeeyu@gmail.com>
2022-05-24 11:00:41 -0700

    napostcard added and now NA page working

--- src/pages/NorthAmericaPage/NorthAmericaPage.jsx
+++ src/pages/NorthAmericaPage/NorthAmericaPage.jsx
@@ -1,10 +1,13 @@
+import { Card, Grid, Icon, Image, Segment } from "semantic-ui-react"
+import { Link } from 'react-router-dom'
+import NaPostCard from "../../components/NaPostCard/NaPostCard"
 
 
 
 function NorthAmericaPage({ northAmerica }) {
     return (
         <>
-        <h1>this is NA page</h1>
+        <NaPostCard northAmerica={northAmerica} />
         </>
     )
 }
