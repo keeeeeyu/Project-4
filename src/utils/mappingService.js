d48fa11dd241b955806cd574f9b6a0c88cb429e8
Kee Yu <keeeeeyu@gmail.com>
2022-05-24 10:20:50 -0700

    asia filter added

--- src/utils/mappingService.js
+++ src/utils/mappingService.js
@@ -4,10 +4,16 @@ function filterEurope(continent) {
     return euData;
 }
 
+function filterAfrica(continent) {
+    const afData = continent.filter(countries => countries.continent === 'Africa')
+}
 
-
+function filterAsia(continent) {
+    const asData = continent.filter(countries => countries.continent === 'Asia')
+}
 const mappingService = {
-  filterEurope
+  filterEurope,
+  filterAfrica,
 };
 
 export default mappingService;
