3a325803526b0ce627e1f598b34963ff9c6661ba
Kee Yu <keeeeeyu@gmail.com>
2022-05-26 10:43:25 -0700

    added header in landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,11 @@
-
+import Header from "../../components/Header/Header"
 
 
 
 export default function LandingPage() {
     return (
         <>
-
+        <Header />
         <h1>this is landing page</h1>
         </>
     )
